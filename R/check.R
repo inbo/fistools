b7f891b04103589a047adae5a0e142e768a811a8
Sander Devisscher <sander.devisscher@inbo.be>
2024-04-29 16:17:55 +0200

    create colcompare
    #7

---
+++ R/check.R
@@ -0,0 +1,13 @@
+#' @author Sander Devisscher
+#'
+#' @description
+#' Helper script to determine existance in environment panel
+#'
+#' @param x environment object
+#'
+#' @returns
+#' 1 = object exists in enviroment
+#' 0 = object doesn't exist in enviroment
+
+
+check <- function(x){tryCatch(if(class(x) == 'logical') 1 else 1, error=function(e) 0)}
