fd41321952167b60a48999cb9f7f121db14de17e
soriadelva <soria.delva@gmail.com>
2024-04-30 11:37:58 +0200

    Update check.R

--- R/check.R
+++ R/check.R
@@ -3,13 +3,13 @@
 #' @author Sander Devisscher
 #'
 #' @description
-#' Helper script to determine existance in environment panel
+#' Helper script to determine existence in environment panel
 #'
 #' @param x environment object
 #'
 #' @returns
-#' 1 = object exists in enviroment
-#' 0 = object doesn't exist in enviroment
+#' 1 = object exists in environment
+#' 0 = object doesn't exist in environment
 
 
 check <- function(x){tryCatch(if(is.logical(class(x))) 1 else 1, error=function(e) 0)}
