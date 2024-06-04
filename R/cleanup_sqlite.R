9961c85aaa089a259c6f8215279774c48acd85a4
Sander Devisscher <sander.devisscher@inbo.be>
2024-06-04 14:14:00 +0200

    document cleanup_sqlite
    #22

--- R/cleanup_sqlite.R
+++ R/cleanup_sqlite.R
@@ -1,7 +1,15 @@
+#' cleanup sqlite
+#'
+#' @param db name of the temporary .sqlite db to be removed
+#'
+#' @description
+#' A helper script to cleanup after use of apply_gtrsdb.
+#'
+
 cleanup_sqlite <- function(db="grts.sqlite"){
-  unlink(db, 
+  unlink(db,
          recursive = TRUE,
          force = TRUE)
-  
+
   file.remove(db)
 }
