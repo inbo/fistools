969ff8f6effff6e7f3128a3843b156f72694ebf0
Sander Devisscher <sander.devisscher@inbo.be>
2024-06-05 18:12:11 +0200

    finish rework
    #22

--- R/apply_grtsdb.R
+++ R/apply_grtsdb.R
@@ -5,50 +5,59 @@
 #' @author Sander Devisscher
 #'
 #' @description
-#' Applies grtsdb from inbo/GRTSdb to a custom perimeter. This function installs
+#' Applies `grtsdb::extract_sample` from inbo/GRTSdb to a custom perimeter. This function installs
 #' GRTSdb if it is missing from your machine.
 #'
 #' @param perimeter a simple features (sf) object
 #' @param cellsize an optional integer. The size of each cell. Either a single value or one value for each dimension. Passed onto extract_sample from GRTSdb. Default is 100.
 #' @param n an optional integer. the sample size. Passed onto extract_sample from GRTSdb. Default is 20
+#' @param export_path an optional character string pointing to the path where the GRTSdb.sqlite is created. Default is "."
 #' @param seed a optional character. Allowing to rerun a previous use.
+#'
+#' @details
+#' GRTSdb is automatically installed when missing from your system.
+#'
 
 apply_grtsdb <- function(perimeter,
                          cellsize = 100,
                          n = 20,
+                         export_path = ".",
                          seed){
   # Setup ####
   ## Libraries ####
-  library(RSQLite)
   tryCatch(library(grtsdb), finally = devtools::install_github("inbo/GRTSdb"))
-  library(rgeos)
-  library(sp)
-  library(tidyverse)
 
-  crs_bel <- CRS("+proj=lcc +lat_1=51.16666723333333 +lat_2=49.8333339 +lat_0=90 +lon_0=4.367486666666666 +x_0=150000.013 +y_0=5400088.438 +ellps=intl +towgs84=-106.869,52.2978,-103.724,0.3366,-0.457,1.8422,-1.2747 +units=m +no_defs")
-  crs_wgs <- CRS("+init=epsg:4326")
+  crs_bel <- "EPSG:31370"
+  crs_wgs <- 4326
   source("../backoffice-wild-analyse/Functies/UUID_List.R")
-  source("./Functions/cleanup_sqlite.R")
 
   ## Checks ####
   ### Perimeter ####
   if(missing(perimeter)){
     stop("perimeter does not exist in global environment")
   }
-  if(class(perimeter) != "SpatialPolygonsDataFrame"){
-    stop(paste0("perimeter is ",
-                class(perimeter),
-                ", not SpatialPolygonsDataFrame"))
+
+  if("SpatialPolygonsDataFrame" %in% class(perimeter)){
+    warning("perimeter is class SpatialPolygonsDataFrame >> converting into sf")
+
+    perimeter <- perimeter %>%
+      sf::st_as_sf()
   }
 
-  projectie <- proj4string(perimeter)
+  projectie <- sf::st_crs(perimeter)
 
   if(is.na(projectie)){
-    stop("De perimeter is niet geprojecteerd, voorzie een projectie. proj4string(perimeter) <- CRS")
+    stop("De perimeter is niet geprojecteerd, voorzie een projectie. Probeer: sf::st_set_crs(perimeter, CRS)")
   }
-  if(projectie != crs_bel@projargs){
+
+  if(projectie$input != crs_bel){
     warning("De perimeter wordt getransfromeerd naar bel_72")
-    perimeter <- spTransform(perimeter, crs_bel)
+    perimeter <- perimeter %>%
+      sf::st_transform(crs_bel)
+  }
+
+  if(nrow(perimeter) > 1){
+    stop("Meer dan 1 polygoon gedetecteerd >> probeer de polygonen te dissolven")
   }
 
   ### n ####
@@ -59,7 +68,15 @@ apply_grtsdb <- function(perimeter,
 
   ## Apply GTRSDB ####
   ### Calculate bbox ####
-  bbox <- as.matrix(perimeter@bbox)
+  temp_bbox <- sf::st_bbox(perimeter) %>%
+    as.data.frame()
+
+  bbox <- matrix(nrow = 2, ncol = 2)
+
+  bbox[1,1] <- temp_bbox$x[1]
+  bbox[2,1] <- temp_bbox$x[2]
+  bbox[1,2] <- temp_bbox$x[3]
+  bbox[2,2] <- temp_bbox$x[4]
 
   ### No seed ####
   if(missing(seed)){
@@ -67,106 +84,97 @@ apply_grtsdb <- function(perimeter,
     seed <- paste(sample(c(letters[1:6],0:9),5,replace=TRUE),collapse="")
     i <- 1
 
-    if(!dir.exists(paste0("./Data/GRTS/", seed))){
-      dir.create(paste0("./Data/GRTS/", seed))
+    export_path <- paste0(export_path, "/", seed)
+
+    if(!dir.exists(export_path)){
+      warning(paste0("Export path is missing >> creating ", export_path))
+      dir.create(export_path)
     }
     #### Check for old db ####
-    if(file.exists("grts.sqlite")){
-      cleanup_sqlite("grts.sqlite")
+    if(file.exists(paste0(export_path, "/grts.sqlite"))){
+      fistools::cleanup_sqlite(paste0(export_path, "/grts.sqlite"))
     }
 
     #### generate new grts.sqlite ####
-    db_name <- paste0("./Data/GRTS/", seed, "/grts.sqlite")
+    db_name <- paste0(export_path, "/grts.sqlite")
 
-    extract_sample(samplesize = n,
-                   bbox = bbox,
-                   cellsize = cellsize)
+    grtsdb::extract_sample(samplesize = n,
+                           bbox = bbox,
+                           cellsize = cellsize)
 
     #### Move db ####
     file.copy(from = "grts.sqlite",
               to = db_name)
 
+    unlink("grts.sqlite")
+
     cleanup_sqlite("grts.sqlite")
 
   }else{
-    db_name <- paste0("./Data/GRTS/", seed, "/grts.sqlite")
+    db_name <- paste0(export_path, "/grts.sqlite")
   }
 
   ### Calculate samplesize of bbox ####
   # Deze waarde is groter dan de maximale samplesize => resulteert in alle
   # Samplepunten binnen de bbox
-  bbox_samplesize <- as.integer(gArea(perimeter)/cellsize)
+  bbox_samplesize <- as.integer(sf::st_area(perimeter)/cellsize^2)
 
   ### Connect to db ####
-  con <- connect_db(db_name)
+  con <- grtsdb::connect_db(db_name)
 
   ### Extract complete sample ####
-  sample <- extract_sample(grtsdb = con,
-                           samplesize = bbox_samplesize,
-                           bbox = bbox,
-                           cellsize = cellsize)
+  sample <- grtsdb::extract_sample(grtsdb = con,
+                                   samplesize = bbox_samplesize,
+                                   bbox = bbox,
+                                   cellsize = cellsize)
 
-  ### Convert sample to sptsdf ####
-  coords <- sample %>%
-    dplyr::select(x1c, x2c)
-
-  all_sample_pts <- SpatialPointsDataFrame(coords,
-                                           sample)
-
-  all_sample_pts$ID_list <- UUID_List(all_sample_pts)
-
-  proj4string(all_sample_pts) <- crs_bel
+  ### Convert sample to sf ####
+  all_sample_pts <- sample %>%
+    sf::st_as_sf(coords = c("x1c", "x2c"),
+                 crs = sf::st_crs(crs_bel))
 
   ### Convert sptsdf to GRID ####
-  sample_grid <- SpatialGridDataFrame(points2grid(all_sample_pts),
-                                      all_sample_pts@data)
+  # 1.adjust the bbox: this ensures the sample points are contained within the cell
+  adjusted_bbox <- sf::st_bbox(all_sample_pts) + c(-cellsize / 2, -cellsize / 2, cellsize / 2, cellsize / 2)
 
-  sample_grid <-  as(sample_grid, "SpatialPolygonsDataFrame")
+  # 2. create a grid
+  sample_grid <- sf::st_make_grid(all_sample_pts,
+                                  cellsize = cellsize,
+                                  offset = c(adjusted_bbox[1], adjusted_bbox[2]))
 
-  proj4string(sample_grid) <- crs_bel
+  # 3. convert grid to sf
+  sample_grid <- sf::st_sf(geometry = sample_grid)
 
-  perimeter_dis <- gUnaryUnion(perimeter)
+  # 4. add ID
+  sample_grid$ID_list = UUID_List(sample_grid)
+
+  # 5. subset gridcells with a sample
+  sample_grid_sub <- sf::st_intersection(sample_grid, all_sample_pts) %>%
+    sf::st_drop_geometry() %>%
+    dplyr::left_join(sample_grid, by = "ID_list") %>%
+    sf::st_as_sf()
 
   ### Select gridcells inside perimeter ####
-  sample_grid_over <- over(sample_grid, perimeter_dis)
-  sample_grid_intersected <- bind_cols(sample_grid_over, sample_grid@data)
-
-  sample_grid_intersected$intersect <- sample_grid_intersected[,1]
-
-  sample_grid_intersected <- sample_grid_intersected %>%
-    filter(!is.na(intersect))
-
-  intersect_ids <- unique(sample_grid_intersected$ID_list)
-
-  sample_grid_intersect <- subset(sample_grid, sample_grid$ID_list %in% intersect_ids)
+  sample_grid_intersect <- sf::st_intersection(perimeter,sample_grid)
 
   ### Select points inside perimeter ####
   # obv de IDs van de geselecteerde gridcells
-  sample_pts_intersect <- raster::intersect(all_sample_pts, as(sample_grid_intersect, "SpatialPolygons"))
+  sample_pts_intersect <- sf::st_intersection(all_sample_pts, sample_grid_intersect)
 
   ### Transform spatial objects ####
-  all_sample_pts <- spTransform(all_sample_pts, crs_wgs)
-  sample_grid_intersect <- spTransform(sample_grid_intersect, crs_wgs)
-  perimeter <- spTransform(perimeter, crs_wgs)
-  sample_pts_intersect <- spTransform(sample_pts_intersect, crs_wgs)
+  all_sample_pts <- sf::st_transform(all_sample_pts, crs_wgs)
+  sample_grid_intersect <- sf::st_transform(sample_grid_intersect, crs_wgs) %>%
+    dplyr::select(ID_list)
+  perimeter <- sf::st_transform(perimeter, crs_wgs)
+  sample_pts_intersect <- sf::st_transform(sample_pts_intersect, crs_wgs) %>%
+    dplyr::select(ID_list, ranking)
 
 
   ## Resample ####
-  final_sample_ranking <- head(sample_pts_intersect$ranking, n)
+  final_sample_ranking <- head(sort(sample_pts_intersect$ranking), n)
 
-  final_samples <- subset(sample_pts_intersect, sample_pts_intersect$ranking %in% final_sample_ranking)
-
-  ### test results (disabled) ####
-#  leaflet(all_sample_pts) %>%
-#    addTiles() %>%
-#    addPolygons(data = sample_grid_intersect) %>%
-#    addPolygons(data = spTransform(perimeter, crs_wgs),
-#                color = "black")  %>%
-#    addPolygons(data = perimeter,
-#                color = "grey") %>%
-#    addCircles(data = sample_pts_intersect,
-#               color = "green") %>%
-#    addMarkers(data = final_samples)
+  final_samples <- sample_pts_intersect %>%
+    dplyr::filter(ranking %in% final_sample_ranking)
 
   ## Return ####
   return(list(seed = seed,
