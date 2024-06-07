90d8ae6534ab327ca26de9c95ea2021f208799fb
Sander Devisscher <sander.devisscher@inbo.be>
2024-06-07 17:16:48 +0200

    start rework
    #30

--- R/label_converter.R
+++ R/label_converter.R
@@ -1,35 +1,196 @@
-label_converter <- function(input, id_column, labelnummer_column, soort_column, labeltype_column, jaar_column){
-  
-  require(tidyverse)
-  
+#' label converter
+#'
+#' @description
+#' Script to convert labelnummer, soort en/of labeltype en jaar into afschotlabel
+#'
+#' @param input a dataframe containing the necessary columns.
+#' @param id_column a character string pointing to a column used to link result with input.
+#' @param labelnummer_column a character string pointing to the column containing label numbers.
+#' @param soort_column a character string pointing to the column containing species.
+#' @param labeltype_column a character string pointing to the column containing label types.
+#' @param jaar_column a character string pointing to the column containing years.
+#' @param output_style a charcter string specifiying the output style. Can be "eloket" or "labo". Default is "eloket".
+#'
+#' @details
+#' The input dataframe should a least contain a id_column & labelnummer_column
+#' other values can be 'hardcoded'.
+#'
+#' @return a dataframe containing 2 columns id & label
+#'
+#' @examples
+#' \dontrun{
+#' }
+#'
+#' @export
+
+
+label_converter <- function(input,
+                            id_column,
+                            labelnummer_column,
+                            soort_column,
+                            labeltype_column,
+                            jaar_column,
+                            output_style = "eloket"){
+  # CHECKS ####
+  ## input is a data.frame ? ####
   if(!is.data.frame(input)){
     stop("input is geen dataframe")
+  }else{
+    temp_input <- input
   }
-  temp_input <- input
-  temp_input$id <- input[[id_column]]
-  temp_input$labelnummer_ruw <- input[[labelnummer_column]]
-  temp_input$soort <- input[[soort_column]]
-  temp_input$labeltype <- input[[labeltype_column]]
-  temp_input$jaar <- input[[jaar_column]]
-  
-  temp_output <- 
-    temp_input %>% 
-    dplyr::select(id, labelnummer_ruw, soort, labeltype, jaar) %>% 
-    mutate(soort = toupper(soort)) %>% 
+
+  ## id_column in input ? ####
+  if(!id_column %in% names(input)){
+    stop(paste0("id_column: ", id_column, " is not present in input"))
+  }else{
+    temp_input$id <- temp_input[[id_column]]
+  }
+
+  ## labelnummer_column in input ? ####
+  if(!labelnummer_column %in% names(input)){
+    stop(paste0("labelnummer_column: ", labelnummer_column, " is not present in input"))
+  }else{
+    temp_input$labelnummer_ruw <- as.integer(temp_input[[labelnummer_column]])
+
+    nonIntLbl <- sum(is.na(temp_input$labelnummer_ruw))
+
+    if(nonIntLbl > 0){
+      warning(paste0(nonIntLbl, " rows have a incorrect labelnumber and will thus be removed"))
+      temp_input <- temp_input %>%
+        dplyr::filter(!is.na(labelnummer_ruw))
+    }
+  }
+
+  ## soort_column ####
+  standard_spec <- c("wild zwijn", "ree", "damhert", "edelhert")
+  bas_zwijn <- c("zwijn", "everzwijn", "boar", "sus scrofa")
+  bas_ree <- c("reegeit", "reebok", "reekits", "smallree", "jaarlingbok", "capreolus capreolus")
+  bas_damhert <- c("dama dama")
+  bas_edelhert <- c("cervus elaphus")
+
+  ### soort_column is in input ? ####
+  if(!soort_column %in% names(input)){
+    warning(paste0("soort_column: ", soort_column, " is not a column of input >> checking if its a allowed species"))
+    ### soort_column is allowed species ? ####
+    if(length(soort_column) > 1){
+      stop(paste0("The soort_column consists of more than 1 species. Add this value to the input dataframe manually. The function has no way to now which species should be used when."))
+    }
+    if(tolower(soort_column) %in% standard_spec){
+      print("soort_column is standard species >> adding to input")
+      temp_input$soort <- toupper(soort_column)
+    }else{
+      if(tolower(soort_column) %in% c(bas_zwijn, bas_ree, bas_damhert, bas_edelhert)){
+        print("soort_column is bastardised")
+        if(tolower(soort_column) %in% bas_zwijn){
+          print(">> adding 'WILD ZWIJN' to input")
+          temp_input$soort <- "WILD ZWIJN"
+        }
+        if(tolower(soort_column) %in% bas_ree){
+          print(">> adding 'REE' to input")
+          temp_input$soort <- "REE"
+        }
+        if(tolower(soort_column) %in% bas_damhert){
+          print(">> adding 'DAMHERT' to input")
+          temp_input$soort <- "DAMHERT"
+        }
+        if(tolower(soort_column) %in% bas_edelhert){
+          print(">> adding 'EDELHERT' to input")
+          temp_input$soort <- "EDELHERT"
+        }
+      }else{
+        stop("soort_column: ", soort_column, " is not a column of input nor is it a allowed species")
+      }
+    }
+  }else{
+    temp_input$soort <- temp_input[[soort_column]]
+
+    temp_input <- temp_input %>%
+      dplyr::mutate(soort = dplyr::case_when(tolower(soort) %in% standard_spec ~ toupper(soort),
+                                             tolower(soort) %in% bas_zwijn ~ "WILD ZWIJN",
+                                             tolower(soort) %in% bas_ree ~ "REE",
+                                             tolower(soort) %in% bas_damhert ~ "DAMHERT",
+                                             tolower(soort) %in% bas_edelhert ~ "EDELHERT",
+                                             TRUE ~ NA_character_))
+
+    warning(paste0(sum(is.na(temp_input$soort)), " rows from input have non valid species & are removed"))
+
+    temp_input <- temp_input %>%
+      dplyr::filter(!is.na(soort))
+  }
+
+
+  ## labeltype_column ####
+  standard_lbltype <- c("reegeit", "reebok", "reekits")
+  ### labeltype_column is in input ? ####
+  if(!labeltype_column %in% names(input)){
+    warning(paste0("labeltype_column: ", labeltype_column, " is not a column of input >> checking if its a allowed labeltype"))
+    if(length(labeltype_column) > 1){
+      stop("The labeltype_column consists of more than 1 labeltype. Add this value to the input dataframe manually. The function has no way to now which labeltype should be used when.")
+    }
+    ### labeltype_column is allowed labeltype ####
+    if(labeltype_column %in% standard_lbltype | !unique(temp_input$soort) == "REE"){
+      temp_input$labeltype <- labeltype_column
+    }else{
+      stop("The labeltype_column is not a column of input nor is it a allowed labeltype.")
+    }
+  }else{
+    temp_input$labeltype <- temp_input[[labeltype_column]]
+
+    temp_input <- temp_input %>%
+      dplyr::mutate(labeltype = dplyr::case_when(tolower(labeltype) %in% standard_lbltype ~ toupper(labeltype),
+                                                 TRUE ~ NA_character_))
+
+    nonStanLblType <- sum(is.na(temp_input$labeltype))-sum(temp_input$soort != "REE")
+
+    if(nonStanLblType > 0){
+      warning(paste0(nonStanLblType, " rows have a non standard labeltype and are thus removed"))
+      temp_input <- temp_input %>%
+        dplyr::filter(soort == "REE" & !is.na(labeltype) | soort != "REE")
+    }
+  }
+
+  ## jaar_column ####
+  ### jaar_column is in input ? ####
+  if(!jaar_column %in% names(input)){
+    warning(paste0("jaar_column: ", jaar_column, " is not a column of input >> checking if its a allowed year"))
+    if(length(jaar_column) > 1){
+      stop("The jaar_column consists of more than 1 year. Add this value to the input dataframe manually. The function has no way to now which year should be used when.")
+    }
+    jaar_column <- as.integer(jaar_column)
+    ### jaar_column is integer & not future ####
+    if(jaar_column >= 2014 & jaar_column <= lubridate::year(Sys.Date())){
+      temp_input$jaar <- jaar_column
+    }else{
+      stop("The jaar_column is neither a column of input nor a valid year (>=2014 & <= current year)")
+    }
+  }else{
+    temp_input$jaar <- input[[jaar_column]]
+  }
+
+  ## output_style is allowed ? ####
+  if(!output_style %in% c("eloket", "labo")){
+    stop("output_style is not one of 'eloket' or 'labo'")
+  }
+
+  # create labels ####
+  temp_output <-
+    temp_input %>%
+    dplyr::select(id, labelnummer_ruw, soort, labeltype, jaar) %>%
+    mutate(soort = toupper(soort)) %>%
     mutate(soort = case_when(soort == "EVERZWIJN" ~ "WILD ZWIJN",
-                             TRUE ~ as.character(soort))) %>% 
+                             TRUE ~ as.character(soort))) %>%
     mutate(labeltype = case_when(soort != "REE" ~ soort,
                                  TRUE ~ case_when(grepl("kits", labeltype, perl = TRUE) ~ paste0(soort,"KITS"),
                                            labeltype == "geit" ~ paste0(soort,"GEIT"),
                                            labeltype == "bok" ~ paste0(soort, "BOK"),
-                                           TRUE ~ as.character(NA)))) %>% 
-    mutate(labelnummer_num = as.numeric(labelnummer_ruw)) %>% 
-    mutate(labelnummer_int = str_pad(labelnummer_num, width = 6, side = "left", pad = 0)) %>% 
-    mutate(labelnummer_pros = paste0("ANB",jaar,labeltype,labelnummer_int)) %>% 
+                                           TRUE ~ as.character(NA)))) %>%
+    mutate(labelnummer_num = as.numeric(labelnummer_ruw)) %>%
+    mutate(labelnummer_int = str_pad(labelnummer_num, width = 6, side = "left", pad = 0)) %>%
+    mutate(labelnummer_pros = paste0("ANB",jaar,labeltype,labelnummer_int)) %>%
     mutate(labelnummer = case_when(!is.na(labelnummer_num) ~ labelnummer_pros,
-                                   TRUE ~ gsub("-", "", labelnummer_ruw))) %>% 
-    filter(!grepl("NA", labelnummer)) %>% 
+                                   TRUE ~ gsub("-", "", labelnummer_ruw))) %>%
+    filter(!grepl("NA", labelnummer)) %>%
     dplyr::select(id, labelnummer)
-  
+
   return(temp_output)
 }
