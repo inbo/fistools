6dff17deb1241efd7b87f72bd88c4e263677ce0c
Sander Devisscher <sander.devisscher@inbo.be>
2024-07-04 17:44:23 +0200

    add documentation + rework labeltype check
    #30

--- R/label_converter.R
+++ R/label_converter.R
@@ -70,40 +70,54 @@ label_converter <- function(input,
 
   ### soort_column is in input ? ####
   if(!soort_column %in% names(input)){
+    #### soort_column is not in input ####
     warning(paste0("soort_column: ", soort_column, " is not a column of input >> checking if its a allowed species"))
-    ### soort_column is allowed species ? ####
+
     if(length(soort_column) > 1){
+      ##### soort_column consists of more than 1 species ####
       stop(paste0("The soort_column consists of more than 1 species. Add this value to the input dataframe manually. The function has no way to now which species should be used when."))
     }
     if(tolower(soort_column) %in% standard_spec){
+      ##### soort_column is standard species ####
+      # > add soort to input with soort_column as value
       print("soort_column is standard species >> adding to input")
       temp_input$soort <- toupper(soort_column)
     }else{
       if(tolower(soort_column) %in% c(bas_zwijn, bas_ree, bas_damhert, bas_edelhert)){
+        #### soort_column is bastardised ####
+        # > add soort to input with equivalent standard species as value
         print("soort_column is bastardised")
         if(tolower(soort_column) %in% bas_zwijn){
+          # soort_column is a bastard form of wild zwijn
           print(">> adding 'WILD ZWIJN' to input")
           temp_input$soort <- "WILD ZWIJN"
         }
         if(tolower(soort_column) %in% bas_ree){
+          # soort_column is a bastard form of ree
           print(">> adding 'REE' to input")
           temp_input$soort <- "REE"
         }
         if(tolower(soort_column) %in% bas_damhert){
+          # soort_column is a bastard form of damhert
           print(">> adding 'DAMHERT' to input")
           temp_input$soort <- "DAMHERT"
         }
         if(tolower(soort_column) %in% bas_edelhert){
+          # soort_column is a bastard form of edelhert
           print(">> adding 'EDELHERT' to input")
           temp_input$soort <- "EDELHERT"
         }
       }else{
+        #### soort_column is not a standard nor bastardised species & soort_column is not in input ####
         stop("soort_column: ", soort_column, " is not a column of input nor is it a allowed species")
       }
     }
   }else{
+    #### soort_column is in input ####
+    # > add soort to input with content of soort_column as value
     temp_input$soort <- temp_input[[soort_column]]
 
+    # > standardise soort
     temp_input <- temp_input %>%
       dplyr::mutate(soort = dplyr::case_when(tolower(soort) %in% standard_spec ~ toupper(soort),
                                              tolower(soort) %in% bas_zwijn ~ "WILD ZWIJN",
@@ -112,40 +126,51 @@ label_converter <- function(input,
                                              tolower(soort) %in% bas_edelhert ~ "EDELHERT",
                                              TRUE ~ NA_character_))
 
-    warning(paste0(sum(is.na(temp_input$soort)), " rows from input have non valid species & are removed"))
+    if(sum(is.na(temp_input$soort)) > 0){
+      #### soort_column has non valid species ####
+      # > remove non valid species
+      temp_input <- temp_input %>%
+        dplyr::filter(!is.na(soort))
+      warning(paste0(sum(is.na(temp_input$soort)), " rows from input have non valid species & are removed"))
 
-    temp_input <- temp_input %>%
-      dplyr::filter(!is.na(soort))
+      if(nrow(temp_input) == 0){
+        #### no valid species in input ####
+        stop("No valid species in input")
+      }
+    }
   }
 
 
   ## labeltype_column ####
   standard_lbltype <- c("reegeit", "reebok", "reekits")
-  ### labeltype_column is in input ? ####
-  if(!labeltype_column %in% names(input)){
-    warning(paste0("labeltype_column: ", labeltype_column, " is not a column of input >> checking if its a allowed labeltype"))
-    if(length(labeltype_column) > 1){
-      stop("The labeltype_column consists of more than 1 labeltype. Add this value to the input dataframe manually. The function has no way to now which labeltype should be used when.")
-    }
-    ### labeltype_column is allowed labeltype ####
-    if(labeltype_column %in% standard_lbltype | !unique(temp_input$soort) == "REE"){
-      temp_input$labeltype <- labeltype_column
+
+  if(unique(temp_input$soort) == "REE"){
+    ### soort is REE ####
+    # in case of REE the labeltype_column should indicate either a column in input or a standard labeltype
+    if(!labeltype_column %in% names(input)){
+      #### labeltype_column is not in input ####
+      # > indicates a value not a column name
+      warning(paste0("labeltype_column: ", labeltype_column, " is not present in input >> checking if its a allowed labeltype"))
+
+      if(length(labeltype_column) > 1){
+        #### labeltype_column consists of more than 1 labeltype ####
+        stop("The labeltype_column consists of more than 1 labeltype. Add this value to the input dataframe manually. The function has no way to now which labeltype should be used when.")
+      }
+
+      if(labeltype_column %in% standard_lbltype){
+        #### labeltype_column is a standard labeltype ####
+        # > all labels will be the same type
+        # > add labeltype to input with labeltype_column as value
+        temp_input$labeltype <- labeltype_column
+      }else{
+        #### labeltype_column is not a standard labeltype & labeltype_column is not in input ####
+        stop("labeltype_column: ", labeltype_column, " is not a column of input nor is it a allowed labeltype")
+      }
     }else{
-      stop("The labeltype_column is not a column of input nor is it a allowed labeltype.")
-    }
-  }else{
-    temp_input$labeltype <- temp_input[[labeltype_column]]
-
-    temp_input <- temp_input %>%
-      dplyr::mutate(labeltype = dplyr::case_when(tolower(labeltype) %in% standard_lbltype ~ toupper(labeltype),
-                                                 TRUE ~ NA_character_))
-
-    nonStanLblType <- sum(is.na(temp_input$labeltype))-sum(temp_input$soort != "REE")
-
-    if(nonStanLblType > 0){
-      warning(paste0(nonStanLblType, " rows have a non standard labeltype and are thus removed"))
-      temp_input <- temp_input %>%
-        dplyr::filter(soort == "REE" & !is.na(labeltype) | soort != "REE")
+      #### labeltype_column is in input ####
+      # > indicates a column name
+      # > add labeltype to input with content of labeltype_column as value
+      temp_input$labeltype <- temp_input[[labeltype_column]]
     }
   }
 
@@ -181,9 +206,9 @@ label_converter <- function(input,
                              TRUE ~ as.character(soort))) %>%
     mutate(labeltype = case_when(soort != "REE" ~ soort,
                                  TRUE ~ case_when(grepl("kits", labeltype, perl = TRUE) ~ paste0(soort,"KITS"),
-                                           labeltype == "geit" ~ paste0(soort,"GEIT"),
-                                           labeltype == "bok" ~ paste0(soort, "BOK"),
-                                           TRUE ~ as.character(NA)))) %>%
+                                                  labeltype == "geit" ~ paste0(soort,"GEIT"),
+                                                  labeltype == "bok" ~ paste0(soort, "BOK"),
+                                                  TRUE ~ as.character(NA)))) %>%
     mutate(labelnummer_num = as.numeric(labelnummer_ruw)) %>%
     mutate(labelnummer_int = str_pad(labelnummer_num, width = 6, side = "left", pad = 0)) %>%
     mutate(labelnummer_pros = paste0("ANB",jaar,labeltype,labelnummer_int)) %>%
