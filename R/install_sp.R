b54a7631e3228b4f9451a61189d35c79144398bc
Sander Devisscher <sander.devisscher@inbo.be>
2024-07-16 17:56:12 +0200

    Update install_sp.R
    #24

--- R/install_sp.R
+++ R/install_sp.R
@@ -5,7 +5,8 @@
 #' @details
 #' The "sp" package should be unloaded from the namespace when it is not needed anymore.
 #' Every function that uses "sp" should start with a call to this function.
-#' And end with a call to `unloadNamespace("sp")`.#'
+#' And end with a call to `unloadNamespace("sp")`.
+#' This is because the "sp" package is known to cause conflicts with other packages.
 #'
 #' @importFrom utils install.packages
 #'
@@ -13,8 +14,16 @@
 #'
 #' @author Sander Devisscher
 
-install_sp <- function() {
+install_sp <- function(force = FALSE) {
   if (!requireNamespace("sp", quietly = TRUE)) {
-    install.packages(, dependencies = TRUE)
+    print("sp is not installed, installing it now")
+    # download the tarball from CRAN https://cran.r-project.org/src/contrib/Archive/sp/sp_2.1-3.tar.gz
+    # and place it in a temporary directory
+    tempfile <- tempfile()
+    download.file("https://cran.r-project.org/src/contrib/Archive/sp/sp_2.1-3.tar.gz", destfile = tempfile)
+
+    # install the tarball
+    install.packages(tempfile, repos = NULL, type = "source", force = force)
   }
+  print("sp is installed \U0001F389")
 }
