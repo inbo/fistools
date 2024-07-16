6be09281b2fedf12fee0728aa8adf30f65ba63d3
Sander Devisscher <sander.devisscher@inbo.be>
2024-07-16 18:36:06 +0200

    Update Class-CRS.R
    #24

--- R/Class-CRS.R
+++ R/Class-CRS.R
@@ -1,4 +1,22 @@
-# Copyright (c) 2003-7 by Barry Rowlingson and Roger Bivand
+#' CRS class definition
+#'
+#' This class is used to store coordinate reference system information for
+#' spatial objects. It is a simple class with a single slot, \code{projargs},
+#' which is a character vector containing the PROJ.4 string representation of
+#' the CRS.
+#'
+#' @slot projargs A character vector containing the PROJ.4 string representation
+#' of the CRS.
+#'
+#' @name CRS-class
+#' @rdname CRS-class
+#' @aliases CRS-class
+#' @keywords classes
+#'
+#' @source sp version 2.1-3 by Edzer Pebesma, Roger Bivand
+#'
+#' @author Copyright (c) 2003-7 by Barry Rowlingson and Roger Bivand
+#' @export
 
 setClass("CRS", slots = c(projargs = "character"),
 # changed to NA_character_ RSB 2020-02-28
