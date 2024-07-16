0723ffa9507c7f0002824ec691b419a13034ebe2
Sander Devisscher <sander.devisscher@inbo.be>
2024-07-16 16:42:42 +0200

    Update lib_crs.R
    #24

--- R/lib_crs.R
+++ R/lib_crs.R
@@ -8,7 +8,6 @@
 #'   \item{CRS_Naam}{ID of the CRS}
 #'   \item{Proj4s}{Javascript Object Notation (JSON) of the CRS}
 #'   \item{ESPG}{EPSG code of the CRS}
-#'   ...
 #' }
 #' @source WGS <https://epsg.io/4326>
 #' @source BEL72 <https://epsg.io/31300>
