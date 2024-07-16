30d5710518abfa322f64ccd38154aeebc180f5d1
Sander Devisscher <sander.devisscher@inbo.be>
2024-07-16 16:23:46 +0200

    Create lib_crs.R
    #24

---
+++ R/lib_crs.R
@@ -0,0 +1,15 @@
+#' lib_crs
+#'
+#' Library of commonly used coordinate reference system (CRS) codes
+#'
+#' @format ## `lib_crs`
+#' is a named list with the following elements:
+#' \describe{
+#'   \item{CRS_Naam}{ID of the CRS}
+#'   \item{Proj4s}{Javascript Object Notation (JSON) of the CRS}
+#'   \item{ESPG}{EPSG code of the CRS}
+#'   ...
+#' }
+#' @source WGS <https://epsg.io/4326>
+#' @source BEL72 <https://epsg.io/31300>
+"lib_crs"
