99f38e07b676ed62ea8e0d9a9aeeb5cb933a2ea9
Sander Devisscher <sander.devisscher@inbo.be>
2024-07-17 12:11:53 +0200

    finish
    #24

--- R/CRS_extracter.R
+++ R/CRS_extracter.R
@@ -1,6 +1,8 @@
 #' CRS_extracter
 #'
 #' Extracts a coordinate reference system (CRS) from a library of commonly used CRS codes
+#' This also fixes issues with the belge lambert 72 we use.
+#' The EPSG code is 31370, but the proj4string is not the same as the one in the EPSG database.
 #'
 #' @param CRS A character string with the name of the CRS
 #' @param EPSG A logical indicating whether the output should be in EPSG format
@@ -9,10 +11,28 @@
 #'
 #' @examples
 #' \dontrun{
+#' # Example of how to use the CRS_extracter function
 #' crs_wgs <- CRS_extracter("WGS", EPSG = FALSE)
-#' crs_bel72 <- CRS_extracter("BEL72", EPSG = FALSE)
+#' crs_bel <- CRS_extracter("BEL72", EPSG = FALSE)
 #'
 #' epsg_wgs <- CRS_extracter("WGS", EPSG = TRUE)
+#'
+#' # Example of how to use the CRS_extracter function in combination with the sf & leaflet packages
+#' library(sf)
+#' library(tidyverse)
+#' library(leaflet)
+#' boswachterijen_df <- boswachterijen$boswachterijen_2024 %>%
+#'   st_transform(crs_bel) %>%
+#'   mutate(centroid = st_centroid(geometry)) %>%
+#'   st_drop_geometry() %>%
+#'   st_as_sf(sf_column_name = "centroid",
+#'            crs = crs_bel) %>%
+#'   st_transform(crs_wgs)
+#'
+#' leaflet() %>%
+#'   addTiles() %>%
+#'   addPolylines(data = boswachterijen$boswachterijen_2024) %>%
+#'   addCircles(data = boswachterijen_df, color = "red", radius = 1000)
 #' }
 #'
 #' @importFrom sp CRS
@@ -20,7 +40,8 @@
 #' @export
 #' @author Sander Devisscher
 
-CRS_extracter <- function(CRS, EPSG = TRUE){
+CRS_extracter <- function(CRS,
+                          EPSG = TRUE){
 
   Lib_CRS <- lib_crs
 
