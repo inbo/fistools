2e0cd7df016e851c4d0c6b5b08d47b419233f500
Sander Devisscher <sander.devisscher@inbo.be>
2025-07-04 14:24:55 +0200

    update code to suppress warnings
    #58

--- R/aggregate_lineparts_sf.r
+++ R/aggregate_lineparts_sf.r
@@ -1,5 +1,6 @@
 #' Connect separate line parts into 1 line
 #'
+#' @description
 #' This function takes a sf object with separate line parts and connects them into 1 line.
 #' The function is based on the st_union function from the sf package.
 #' The function is designed to work with sf objects that have a column with unique
@@ -42,146 +43,155 @@
 #' plot
 #' }
 
-aggregate_lineparts_sf <- function(sf_data,
-                                   sf_id){
-
-  # check if sf_data is a sf object
-  if(!inherits(sf_data, "sf")){
+aggregate_lineparts_sf <- function(sf_data, sf_id) {
+  # Check if sf_data is a sf object
+  if (!inherits(sf_data, "sf")) {
     stop("sf_data is not a sf object")
   }
 
-  # check if sf_id is a character string
-  if(!is.character(sf_id)){
+  # Check if sf_id is a character string
+  if (!is.character(sf_id)) {
     warning("sf_id is not a character string >> converting to character string")
     sf_id <- as.character(sf_id)
   }
 
-  # check if sf_id is a column in sf_data
-  if(!(sf_id %in% names(sf_data))){
+  # Check if sf_id is a column in sf_data
+  if (!(sf_id %in% names(sf_data))) {
     stop("sf_id is not a column in sf_data")
   } else {
-    # check if sf_id == "sf_id"
-    if(sf_id != "sf_id"){
+    # If the column is not already called 'sf_id', create it
+    if (sf_id != "sf_id") {
       sf_data <- sf_data %>%
-        dplyr::mutate(sf_id = as.character(sf_data[[sf_id]]))
+        dplyr::mutate(sf_id = as.character(.data[[sf_id]]))
     }
   }
 
-  # check if geometry is present
-  if(!"geometry" %in% names(sf_data)){
+  # Ensure geometry column is present
+  if (!"geometry" %in% names(sf_data)) {
     sf_data <- sf_data %>%
       dplyr::mutate(geometry = sf::st_geometry(.))
   }
 
-  # get unique sf_ids
+  # Get unique sf_ids
   sf_ids <- unique(sf_data$sf_id)
-
   output <- data.frame()
 
-  for(i in sf_ids){
-    # get line parts with the same sf_id
+  for (i in sf_ids) {
+    # Get line parts with the same sf_id
     sf_unit <- sf_data %>%
       dplyr::filter(sf_id == i) %>%
       sf::st_union(by_feature = FALSE) %>%
       sf::st_cast("LINESTRING")
 
-    # create empty data frame to store points
+    # Create empty data frame to store points
     temp <- data.frame() %>%
-      dplyr::mutate(lon = NA_integer_,
-                    lat = NA_integer_)
+      dplyr::mutate(lon = NA_real_, lat = NA_real_)
 
-    # loop over line parts to convert them to points
-    for(n in 1:length(sf_unit)){
+    # Loop over line parts to convert them to points
+    for (n in 1:length(sf_unit)) {
       temp <- temp %>%
-        dplyr::add_row(lon = sf_unit[[n]][,1],
-                       lat = sf_unit[[n]][,2]) %>%
+        dplyr::add_row(
+          lon = sf_unit[[n]][, 1],
+          lat = sf_unit[[n]][, 2]
+        ) %>%
         dplyr::distinct(lon, lat) %>%
         dplyr::arrange(lat, lon)
 
       sf_point <- temp %>%
         sf::st_as_sf(coords = c("lon", "lat"))
 
-      # order points
+      # Order points
       sf_point_ordered <- data.frame()
-
       a <- 1
 
-      # start progress bar
-      pb <- progress::progress_bar$new(format = "  [:bar] :percent ETA: :eta",
-                                       total = nrow(sf_point),
-                                       clear = FALSE,
-                                       width = 60)
+      # Progress bar (optional, can be removed if not needed)
+      pb <- progress::progress_bar$new(
+        format = "  [:bar] :percent ETA: :eta",
+        total = nrow(sf_point),
+        clear = FALSE,
+        width = 60
+      )
 
-      while(a <= nrow(sf_point)){
-        # tick progress bar
+      while (a <= nrow(sf_point)) {
         pb$tick()
 
-        if(a == 1){
-          # get first point
-          sf_point_ref <- sf_point[a,]
-          # select the other points of the linepart
-          outside <- sapply(sf::st_intersects(sf_point, sf_point_ref),function(x){length(x)==0})
+        if (a == 1) {
+          # Get first point
+          sf_point_ref <- sf_point[a, ]
+          # Select the other points of the linepart
+          outside <- as.vector(sapply(
+            sf::st_intersects(sf_point, sf_point_ref),
+            function(x) length(x) == 0
+          ))
           sf_point_comp <- sf_point[outside, ]
+          # Calculate distance between points
+          sf_point_comp$distance <- as.vector(sf::st_distance(sf_point_comp, sf_point_ref))
 
-          # calculate distance between points
-          sf_point_comp$distance <- sf::st_distance(sf_point_comp, sf_point_ref)
+          # Check if there are points to compare
+          if (nrow(sf_point_comp) > 0 && any(!is.na(sf_point_comp$distance))) {
+            min_dist <- min(sf_point_comp$distance, na.rm = TRUE)
+            sf_point_new_ref <- sf_point_comp %>%
+              dplyr::filter(distance == min_dist) %>%
+              dplyr::select(geometry)
+          } else {
+            break
+          }
 
-          # get the point with the smallest distance
-          sf_point_new_ref <- as.data.frame(sf_point_comp) %>%
-            sf::st_as_sf() %>%
-            dplyr::mutate(min = min(distance, na.rm = TRUE)) %>%
-            dplyr::filter(distance == min) %>%
-            dplyr::select(geometry)
-
-          # add the new point to the ordered points
+          # Add the new point to the ordered points
           sf_point_ordered <- rbind(sf_point_ref, sf_point_new_ref)
-
           a <- a + 1
 
-        }else{
-          # get the other points of the linepart
+        } else {
+          # Get the other points of the linepart
           sf_point_ref <- sf_point_new_ref
-          # select the other points of the linepart
-          outside <- sapply(sf::st_intersects(sf_point, sf_point_ordered),function(x){length(x)==0})
-          # calculate distance between points
+          # Select the other points of the linepart
+          outside <- as.vector(sapply(
+            sf::st_intersects(sf_point, sf_point_ordered),
+            function(x) length(x) == 0
+          ))
           sf_point_comp <- sf_point[outside, ]
-          # calculate distance between points
-          sf_point_comp$distance <- sf::st_distance(sf_point_comp, sf_point_ref)
-          # get the point with the smallest distance
-          sf_point_new_ref <- as.data.frame(sf_point_comp) %>%
-            sf::st_as_sf() %>%
-            dplyr::mutate(min = min(distance, na.rm = TRUE)) %>%
-            dplyr::filter(distance == min) %>%
-            dplyr::select(geometry)
-          # add the new point to the ordered points
-          sf_point_ordered <- rbind(sf_point_ordered, sf_point_new_ref)
+          # Calculate distance between points
+          sf_point_comp$distance <- as.vector(sf::st_distance(sf_point_comp, sf_point_ref))
 
+          # Check if there are points to compare
+          if (nrow(sf_point_comp) > 0 && any(!is.na(sf_point_comp$distance))) {
+            min_dist <- min(sf_point_comp$distance, na.rm = TRUE)
+            sf_point_new_ref <- sf_point_comp %>%
+              dplyr::filter(distance == min_dist) %>%
+              dplyr::select(geometry)
+          } else {
+            break
+          }
+
+          # Add the new point to the ordered points
+          sf_point_ordered <- rbind(sf_point_ordered, sf_point_new_ref)
           a <- a + 1
         }
       }
 
-      # convert ordered points to linestring
+      # Convert ordered points to linestring
       test_sf <- sf_point_ordered %>%
         sf::st_coordinates() %>%
         sf::st_linestring() %>%
         sf::st_geometry()
     }
 
-    # add sf_id to the linestring
+    # Add sf_id to the linestring
     test_sf2 <- as.data.frame(test_sf) %>%
       dplyr::mutate(sf_id = i)
 
-    # add linestring to output
-    if(nrow(output) == 0){
+    # Add linestring to output
+    if (nrow(output) == 0) {
       output <- test_sf2
-    }else{
+    } else {
       output <- rbind(output, test_sf2)
     }
   }
 
-  # convert output to sf object
+  # Convert output to sf object
   output <- output %>%
     sf::st_as_sf()
 
   return(output)
 }
+
