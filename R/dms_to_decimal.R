93fa49ff846f1b464798c19176b893b7fba24b39
Sander Devisscher <sander.devisscher@inbo.be>
2025-07-25 14:23:49 +0200

    Create dms_to_decimal.R
    #117

---
+++ R/dms_to_decimal.R
@@ -0,0 +1,77 @@
+#' @title dms_to_decimal
+#'
+#' @description
+#' Convert degrees, minutes, and seconds to decimal degrees.
+#'
+#' @param deg Degrees (numeric or character).
+#' @param min Minutes (numeric or character).
+#' @param sec Seconds (numeric or character).
+#' @param direction Direction (character, optional, e.g., 'N', 'S', 'E', 'W').
+#'
+#' @return Decimal degrees (numeric).
+#' @family spatial
+#' @export
+#'
+#' @author Sander Devisscher
+#'
+#' @examples
+#' \dontrun{
+#' # Convert 30\u00B0 15'50"N or 30 degrees, 15 minutes, and 50 seconds north to decimal degrees
+#' dms_to_decimal(30, 15, 50, 'N')
+#' # Returns 30.26389
+#' }
+dms_to_decimal <- function(deg, min, sec, direction = NULL) {
+  dec <- as.numeric(deg) + as.numeric(min)/60 + as.numeric(sec)/3600
+  if (!is.null(direction)) {
+    if (direction %in% c('S', 'W')) dec <- -dec
+  }
+  return(dec)
+}
+
+#' @title dms column_to_decimal
+#'
+#' @description
+#' Convert a data frame column with degrees, minutes as well as seconds to decimal degrees.
+#'
+#' @param df Data frame containing the DMS columns.
+#' @param coord_col Name of the column with coordinates in degrees.
+#'
+#' @return Data frame with a new column containing decimal degrees.
+#' @export
+#'
+#' @author Sander Devisscher
+#' @family spatial
+#'
+#' @examples
+#' \dontrun{
+#' # Example data frame
+#' df <- data.frame(
+#'  lat_Y = c("50\u00B045'38.7\"N", "50\u00B046'04.9\"N", "50\u00B045'19.7\"N",
+#'  "50\u00B045'18.0\"N", "50\u00B046'24.4\"N", "50\u00B045'29.2\"N"),
+#'  long_X = c("3\u00B010'26.0\"E", "3\u00B010'28.0\"E", "3\u00B010'25.0\"E",
+#'  "3\u00B010'24.0\"E", "3\u00B010'30.0\"E", "3\u00B010'27.0\"E"))
+#'
+#' # Convert the lat_Y and long_X columns to decimal degrees
+#' df <- dms_column_to_decimal(df, c("lat_Y", "long_X"))
+#' # View the updated data frame
+#' print(df)
+#' }
+
+dms_column_to_decimal <- function(df, coord_col) {
+  for (col in coord_col) {
+    if (!col %in% names(df)) {
+      stop(paste("Column", col, "not found in the data frame."))
+    }
+
+    # Extract degrees, minutes, seconds, and direction
+    dms <- strsplit(as.character(df[[col]]), "[\u00B0 '\" ]")
+    df[[paste0(col, "_decimal")]] <- sapply(dms, function(x) {
+      deg <- as.numeric(x[1])
+      min <- as.numeric(x[2])
+      sec <- as.numeric(x[3])
+      direction <- x[4]
+      dms_to_decimal(deg, min, sec, direction)
+    })
+  }
+  return(df)
+}
