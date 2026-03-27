93b3f1b2365430cfc04cc785512e8dd889bbafcf
Sander Devisscher <sander.devisscher@inbo.be>
2026-03-27 17:49:58 +0100

    add docu
    #148

--- R/validate_agouti_ai.R
+++ R/validate_agouti_ai.R
@@ -1,3 +1,32 @@
+#' Validate AI classifications in Agouti
+#'
+#' @description
+#' This function allows you to validate AI classifications in Agouti by
+#' downloading a datapackage from Google Drive, filtering observations based on
+#' the specified AI model and species, and opening the relevant sequence IDs in
+#' Agouti for manual validation.
+#'
+#' @param gfileID The Google Drive file ID of the datapackage to be downloaded.
+#' @param ai_model The name of the AI model used for classification (e.g., "Europe"). Default is "Europe".
+#' @param agouti_prj_id The Agouti project ID where the sequences will be opened for validation.
+#' @param species Optional. A vector of species (scientific names) to filter the
+#' observations for validation. If NULL, the user will be prompted to select species
+#' from the available options.
+#' @param email Optional. The email address used for authentication when downloading the
+#' datapackage from Google Drive. If not provided, the function will attempt to
+#' retrieve it from system environment variables or prompt the user for input.
+#'
+#' @details
+#' The function performs the following steps:
+#' 1. Checks for the required parameters and prompts the user if necessary.
+#' 2. Downloads the specified datapackage from Google Drive using the provided file ID.
+#' 3. Unzips the downloaded datapackage and reads it using the `camtraptor` package.
+#' 4. Filters the observations based on the specified AI model and species.
+#' 5. Extracts the unique sequence IDs from the filtered observations and opens them in Agouti for manual validation using the `agouti_imager` function.
+#' 6. The user can validate the classifications in Agouti
+#' *NOTE:* This function doesn't track which sequences have been validated!
+#'
+#'
 validate_agouti_ai <- function(gfileID,
                                ai_model = "Europe",
                                agouti_prj_id,
