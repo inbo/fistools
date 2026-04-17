582a35f263de31af03601e7db8ec65d54ce03f43
Sander Devisscher <sander.devisscher@inbo.be>
2026-04-17 18:18:58 +0200

    add docu
    #154

--- R/read_dmogg.R
+++ R/read_dmogg.R
@@ -1,3 +1,39 @@
+#' Read "dieren met onderkaakgegevens georef"
+#'
+#' This function reads the most up to date version of the
+#' dieren met onderkaakgegevens georef (DMOGG) file from the backoffice wild
+#' analyse.
+#'
+#' @param type "short" or "long" indicating which version you want to import.
+#' The default is "short". See details for more info.
+#' @param email email used to authenticate the google drive
+#'
+#' @details
+#' Two versions of the DMOGG dataset can be imported by this function, namely:
+#' - The "Short" version containing only data collected via the e-loket and later
+#' WIZ-app from ANB enriched with data from the autopsiedb.
+#' The temporal range of this dataset is 2014 - today.
+#' This dataset is more complete and contains spatial data concerning wbe's & fbz's.
+#' - The "Long" version containing the data from the "Short" dataset plus data
+#' collected via the predecessors of the e-loket (ao zwijntje) by INBO.
+#' This dataset is less complete because early data (<2014) misses almost all spatial info.
+#'
+#' This function uses `fistools::download_gdrive_if_missing()` under the hood to
+#' download the file from the google drive.
+#'
+#' @returns a dataframe containing the most recent DMOGG data.
+#'
+#' @family download
+#' @export
+#'
+#' @author Sander Devisscher
+#' @examples
+#' \dontrun{
+#' # read the most recent short DMOGG data
+#' dmogg <- read_dmogg()
+#' }
+#'
+
 read_dmogg <- function(type = "short",
                        email = Sys.getenv("email")){
 
