9472765e2379142b5e63b27560a02ed4df5d1249
Sander Devisscher <sander.devisscher@inbo.be>
2026-08-05 14:58:45 +0200

    add example
    #170

--- R/redo_non_tracked.R
+++ R/redo_non_tracked.R
@@ -51,7 +51,51 @@
 #'
 #' @examples
 #' \dontrun{
+#' zip_dir <- tempdir()
+#' files_dir <- paste0(zip_dir, "\\Files\\")
 #'
+#' download_gdrive_if_missing(gfileID = "1U7cSwhUZRJlR_zpJvYyhsi24J2QV8U8q",
+#'                            destfile = file.path(zip_dir, "gmu8.zip"),
+#'                            update_always = TRUE,
+#'                            email = Sys.getenv("email"))
+#'
+#' unzip(paste0(zip_dir, "/gmu8.zip"),
+#'       exdir = file.path(files_dir))
+#'
+#' gmu8 <- camtrapdp::read_camtrapdp(paste0(files_dir, "/datapackage.json"))
+#'
+#' calibration_deps <- gmu8 %>%
+#'   camtrapdp::filter_observations(cameraSetupType == "calibration")
+#'
+#' obs_list <- calibration_deps$data$observations
+#' dep_list <- unique(obs_list$deploymentID)
+#'
+#' data <- gmu8 |>
+#'   camtrapdp::filter_deployments(deploymentID %in% dep_list)
+#'
+#' track_data <- data %>%
+#'   camtrapdp::filter_observations(scientificName %in% "Sus scrofa")
+#'
+#' file_path <- "../fis-rem-tracking/GMU8/Input/tracking_seq_done.csv"
+#'
+#' seq_done <- tryCatch({
+#'   # Attempt to read the CSV file
+#'   read.csv(file_path)
+#' }, error = function(e) {
+#'   # If an error occurs (e.g., file not found), create the dummy data frame
+#'   data.frame(sequenceID = character())
+#' })
+#'
+#' seq_done <- c(seq_done$sequenceID)
+#'
+#' redo_non_tracked <- redo_non_tracked(track_data = track_data,
+#'                                      seq_done_in = seq_done)
+#'
+#' seq_done_out <- redo_non_tracked$seq_done_out
+#' parsing_issues <- redo_non_tracked$non_tracked_parsing_error
+#' toofew_poles <- redo_non_tracked$non_tracked_toofew_poles
+#' toofew_positions <- redo_non_tracked$non_tracked_toofew_positions
+#' other_issues <- redo_non_tracked$non_tracked_other_issues
 #' }
 
 redo_non_tracked <- function(track_data,
