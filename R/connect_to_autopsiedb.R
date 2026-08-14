3369c254a9911964278e7d851b6d80d734808ef1
Sander Devisscher <sander.devisscher@inbo.be>
2026-08-14 10:26:11 +0200

    rework autopsiedb
    #172

--- R/connect_to_autopsiedb.R
+++ R/connect_to_autopsiedb.R
@@ -1,21 +1,82 @@
-connect_to_autopsiedb <- function(table,
-                                  connectie_type = "inbo-uat",
-                                  role = "inbo-developers-fis-role"){
+#' Connect to autopsiedb
+#'
+#' After the upgrade to a newer version of postgresql connecting to the
+#' autopsiedb no longer occurs via the bastion but via a modified localhost.
+#'
+#' @param name description
 
+
+connect_to_autopsiedb <- function(table,
+                                  db = "2",
+                                  connectie_type = "inbo-prod",
+                                  role = "inbo-autopsies-rds-connect-role",
+                                  devops_toolkit_path = "../../../bin/devops-toolkit-windows-amd64.exe"){
+
+  devops_toolkit_path <- normalizePath(devops_toolkit_path)
+
+  # Test prep ####
+  if(file.exists(devops_toolkit_path)){
+    cat("The devops-toolkit is installed!")
+  }else{
+    stop(paste0("The devops-toolkit is not installed at ", devops_toolkit_path,
+                " \n points to the wrong file. Try reïnstalling the devops-toolkit, or changing the devops_toolkit_path variable"))
+  }
+
+  # create sessiontoken ####
+  ## MFA ####
+  mfa_code <- svDialogs::dlg_input("Enter MFA Code: ")$res
+
+  ## CMD ####
+  cmd <- paste0(devops_toolkit_path, ' aws-mfa -u ',
+                Sys.getenv("USERNAME"),
+                ' -a ', connectie_type, ' -r ', role)
+  ## execute ####
+  system(
+    command = cmd,
+    input = mfa_code,
+    ignore.stdout = FALSE,
+    ignore.stderr = FALSE,
+    wait = TRUE
+  )
+
+  ## store profile ####
   aws_profile <- paste0(connectie_type, "-",
                         strsplit(Sys.getenv("USERNAME"), split = "-"))
   aws_profile <- gsub(pattern = "_", replacement = "-", x = aws_profile)
 
-  Sys.setenv("AWS_PROFILE" = aws_profile)
+  Sys.setenv("AWS_DEFAULT_PROFILE" = aws_profile)
 
-  mfa_code <- svDialogs::dlg_input("Enter MFA Code: ")$res
-  cmd <- paste0(normalizePath("../../../bin/aws-cli-mfa-login.exe"), ' aws-mfa -u ',
-                Sys.getenv("USERNAME"),
-                ' -a ', connectie_type, ' -r ', role)
+  # Connect to the autopsiedb ####
+  ## DB checks ####
+  ### is character? ####
+  if(!is.character(db)){
+    db <- as.character(db)
+  }
 
-  system(cmd, input = mfa_code, intern = FALSE, ignore.stdout = FALSE, ignore.stderr = FALSE, wait = TRUE)
+  ### is provided ####
+  if(is.null(db) | is.na(db)){
+    cmd3 <- paste0(devops_toolkit_path,
+                   ' rds-list -p ', aws_profile)
 
-  cmd <- paste0(normalizePath("../../../bin/rds-discover.exe"), 'devops-toolkit rds-list -p', aws_profile, '')
+    db <- svDialogs::dlg_input(paste0("No db provided use the number of one
+                                            of these: ", system(
+      command = cmd3,
+      intern = FALSE,
+      ignore.stdout = FALSE,
+      ignore.stderr = FALSE,
+      wait = TRUE
+    )))$res
+  }
 
-  system(cmd, intern = FALSE, ignore.stdout = FALSE, ignore.stderr = FALSE, wait = TRUE)
+  ## Connection ####
+  cmd2 <- paste0(devops_toolkit_path,
+                 ' rds-connect -l 5555 -p ', aws_profile)
+
+  system(
+    command = cmd2,
+    input = db,
+    ignore.stdout = FALSE,
+    ignore.stderr = FALSE,
+    wait = TRUE
+  )
 }
